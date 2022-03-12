59cd9dc4edc7320b9b7ae60556b497673426cd76
Thomas Kelly <thom.j.kelly@gmail.com>
2022-03-12 15:46:20 -0600

    Initial commit

---
+++ lib/learner-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class LearnerStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'LearnerQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
